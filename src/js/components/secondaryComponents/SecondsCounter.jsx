88ce974e2ff989c34263e44a454987be67d8eb42
trivmvirat <consvpr@outlook.es>
2025-07-11 05:00:08 +0000

    ultimo

--- src/js/components/secondaryComponents/SecondsCounter.jsx
+++ src/js/components/secondaryComponents/SecondsCounter.jsx
@@ -1,16 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
+import { faHourglassEnd } from '@fortawesome/free-solid-svg-icons';
 
-const TimeCounter = ({ totalSeconds }) => {
-  const hours = Math.floor(totalSeconds / 3600);
-  const minutes = Math.floor((totalSeconds % 3600) / 60);
-  const seconds = totalSeconds % 60;
 
-  // Para mostrar siempre dos dígitos
-  const format = (val) => String(val).padStart(2, '0');
+const TimeCounter = () => {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const secs = seconds % 60;
+
+  const format = (val) => String(val).padStart(2, "0");
 
   return (
-    <div className="text-center" style={{ fontSize: "2rem" }}>
-      <p>{format(hours)}:{format(minutes)}:{format(seconds)}</p>
+    <div className="container d-flex align-items-center justify-content-center">
+      <div className="row">
+        <div className="col-md-4 border bg-black text-white d-flex align-items-center justify-content-center  " style={{width:"100px",height:"100px",color:"white",fontSize:"3rem"}}>
+          <FontAwesomeIcon icon={faClock} />
+        </div>
+        <div className="col-md-4 border bg-black text-white d-flex align-items-center justify-content-center  " style={{width:"100px",height:"100px",color:"white",fontSize:"3rem"}}>
+
+        {format(hours)}
+      </div>
+      <div className="col-md-4 border bg-black text-white  d-flex align-items-center justify-content-center" style={{width:"100px",height:"100px",color:"white",fontSize:"3rem"}}>
+
+        {format(minutes)}
+      </div>
+      <div className="col-md-4 border bg-black text-white  d-flex align-items-center justify-content-center" style={{width:"100px",height:"100px",color:"white", fontSize:"3rem"}}>
+
+        {format(secs)}
+      </div>
+      </div>
+      
+     
+      
     </div>
   );
 };
